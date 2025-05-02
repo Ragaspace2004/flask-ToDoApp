634061456546eb3e3894e325fcbe89c81480b009
ragaspace2004 <ragapriyakarthikeyan2004@gmail.com>
2025-05-02 18:40:54 +0530

    Added sql statement

--- index.py
+++ index.py
@@ -16,6 +16,9 @@ class Todo(db.Model):
   
 @app.route('/', methods=['POST','GET'])
 def index():
+  user_input = "admin' OR '1'='1"
+  query = f"SELECT * FROM users WHERE username = '{user_input}'"
+  print(query)
   if request.method=='POST':
     task_content=request.form['content']
     new_task=Todo(content=task_content)
